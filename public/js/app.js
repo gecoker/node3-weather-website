1a156ceff7611442e325360c31bf2e270513e514
Grady Coker <gcoker@rhainc.com>
2019-05-28 11:52:09 -0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,13 +11,11 @@ const addressInput = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     setText('', '')
     const address = addressInput.value
-    fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+    fetch('/weather?address=' + address).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 setText('Error: ' + data.error, '')
